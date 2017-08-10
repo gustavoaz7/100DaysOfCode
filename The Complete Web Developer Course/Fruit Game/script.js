7adc38282ad2af418cbc0a8a85db449d04514f95
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-09 23:48:46 -0300

    on progress

--- The Complete Web Developer Course/Fruit Game/script.js
+++ The Complete Web Developer Course/Fruit Game/script.js
@@ -8,13 +8,28 @@ const start = document.querySelector('#start')
 const gameover = document.querySelector('#gameOver')
 const finalScore = document.querySelector('#finalScore')
 
+const fruits = [
+	'https://vignette3.wikia.nocookie.net/fruitninja/images/1/16/Kiwi_Fruit.svg',
+	'https://vignette2.wikia.nocookie.net/fruitninja/images/0/07/Pear.svg',
+	'https://vignette3.wikia.nocookie.net/fruitninja/images/e/e2/Strawberry.svg',
+	'https://vignette1.wikia.nocookie.net/fruitninja/images/d/d1/Cherry.svg',
+	'https://vignette2.wikia.nocookie.net/fruitninja/images/4/43/Orange.png',
+	'https://vignette2.wikia.nocookie.net/fruitninja/images/4/43/Orange.png',
+	'https://vignette1.wikia.nocookie.net/fruitninja/images/e/e6/Plum.png',
+	'https://vignette4.wikia.nocookie.net/fruitninja/images/3/34/Lime.png'
+];
+let lastFruit;
+
 scoreVal.textContent = 0;
 
 
 function startGame() {
 	AnimateInstr();
 	setTimeout(function() {
-	console.log('a')
+	styleStart();
+	setInterval(function() {
+		instruction.append(`<img src="${pickFruit()}" >`);
+	}, 1000)
 
 
 
@@ -43,4 +58,21 @@ function AnimateInstr() {
 	setTimeout(() => instruction.classList.remove('active'), 1000)
 }
 
+function styleStart() {
+	start.querySelector('p').textContent = 'Reset Game';
+	start.style.transform = 'scale(0.5)';
+	start.style.bottom = '0%';
+}
+
+function pickFruit() {
+	let i = fruits[Math.round(Math.random()*8)];
+	if (lastFruit !== i) {
+		lastFruit = i;
+		return i;
+	}
+	pickFruit();
+}
+
 start.addEventListener('click', startGame)
+
+
