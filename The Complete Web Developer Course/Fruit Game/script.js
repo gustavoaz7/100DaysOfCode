4fc3876f9d7c11517597c95569a1c534a3432cae
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-11 14:20:26 -0300

    on progess

--- The Complete Web Developer Course/Fruit Game/script.js
+++ The Complete Web Developer Course/Fruit Game/script.js
@@ -7,33 +7,41 @@ const instruction = document.querySelector('#instruction')
 const start = document.querySelector('#start')
 const gameover = document.querySelector('#gameOver')
 const finalScore = document.querySelector('#finalScore')
-
+const fruit = document.querySelector('.fruit')
 const fruits = [
 	'https://vignette3.wikia.nocookie.net/fruitninja/images/1/16/Kiwi_Fruit.svg',
 	'https://vignette2.wikia.nocookie.net/fruitninja/images/0/07/Pear.svg',
 	'https://vignette3.wikia.nocookie.net/fruitninja/images/e/e2/Strawberry.svg',
 	'https://vignette1.wikia.nocookie.net/fruitninja/images/d/d1/Cherry.svg',
 	'https://vignette2.wikia.nocookie.net/fruitninja/images/4/43/Orange.png',
-	'https://vignette2.wikia.nocookie.net/fruitninja/images/4/43/Orange.png',
 	'https://vignette1.wikia.nocookie.net/fruitninja/images/e/e6/Plum.png',
 	'https://vignette4.wikia.nocookie.net/fruitninja/images/3/34/Lime.png'
 ];
 let lastFruit;
+let step = 0;
+let trials = 3;
 
 scoreVal.textContent = 0;
 
 
 function startGame() {
 	AnimateInstr();
-	setTimeout(function() {
-	styleStart();
-	setInterval(function() {
-		instruction.append(`<img src="${pickFruit()}" >`);
-	}, 1000)
-
-
-
-
+	setTimeout(() => {
+		styleStart();
+		positionFruit();
+		setInterval(() => {
+			step += 1 + Math.round(Math.random()*4);
+			fruit.style.top = `${-fruit.offsetHeight + step}px`;
+			if (fruit.offsetTop >= game.offsetHeight) {
+				if (trials > 0) {
+					trials--;
+					life.lastElementChild.remove();
+					step = 0;
+					return positionFruit();
+				}
+				return gameOverF();
+			}
+		}, 10)
 
 
 	}, 1250)
@@ -65,12 +73,27 @@ function styleStart() {
 }
 
 function pickFruit() {
-	let i = fruits[Math.round(Math.random()*8)];
-	if (lastFruit !== i) {
-		lastFruit = i;
-		return i;
-	}
-	pickFruit();
+	let i = Math.floor(Math.random()*fruits.length);
+	if (lastFruit === fruits[i]) pickFruit();
+	lastFruit = fruits[i];
+	return fruits[i];
+}
+
+function horizPosition() {
+	let area = game.getBoundingClientRect();
+	let item = fruit.getBoundingClientRect();
+	return Math.random()*(area.width - item.width);
+}
+
+function positionFruit() {
+	fruit.setAttribute('src', pickFruit())
+	fruit.style.left = `${horizPosition()}px`;
+	fruit.style.top = `-${fruit.offsetHeight}px`;
+	// $(#game).append(`<img class="fruit" src="${pickFruit()}" >`)  -  This method allows to appear more than one fruit in game at the same time
+}
+
+function gameOverF() {
+
 }
 
 start.addEventListener('click', startGame)
