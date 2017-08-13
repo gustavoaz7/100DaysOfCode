2001b80d52c616b3654085c8c912238c79a70ea2
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-13 18:24:04 -0300

    on progress

--- The Complete Web Developer Course/Fruit Game/script.js
+++ The Complete Web Developer Course/Fruit Game/script.js
@@ -8,6 +8,7 @@ const start = document.querySelector('#start')
 const gameover = document.querySelector('#gameOver')
 const finalScore = document.querySelector('#finalScore')
 const fruit = document.querySelector('.fruit')
+const slicesound = document.querySelector('#slicesound')
 const fruits = [
 	'https://vignette3.wikia.nocookie.net/fruitninja/images/1/16/Kiwi_Fruit.svg',
 	'https://vignette2.wikia.nocookie.net/fruitninja/images/0/07/Pear.svg',
@@ -17,37 +18,37 @@ const fruits = [
 	'https://vignette1.wikia.nocookie.net/fruitninja/images/e/e6/Plum.png',
 	'https://vignette4.wikia.nocookie.net/fruitninja/images/3/34/Lime.png'
 ];
-let lastFruit;
-let step = 0;
-let trials = 3;
+let lastFruit, step, trials, scoreCount = 0;
 
-scoreVal.textContent = 0;
+scoreVal.textContent = scoreCount;
 
 
 function startGame() {
-	AnimateInstr();
+	// Setting up starting configuration
+	trials = 3;
+	step = 0;
+	scoreCount = 0;
+	while (life.childElementCount < 3) $('#life').append('<img src="http://icons.iconarchive.com/icons/succodesign/love-is-in-the-web/48/heart-icon.png" alt="">');
+	// Removing any configuration from previous games
+	if (gameover.classList) {
+		gameover.classList.remove('active');
+		game.style.filter = 'grayscale(0%)';
+	}
+	clearInterval(fruitDown)
+
+	// Animating introduction
+	animateInstr();
+	// Game starts after the initial animation
 	setTimeout(() => {
 		styleStart();
 		positionFruit();
-		setInterval(() => {
-			step += 1 + Math.round(Math.random()*4);
-			fruit.style.top = `${-fruit.offsetHeight + step}px`;
-			if (fruit.offsetTop >= game.offsetHeight) {
-				if (trials > 0) {
-					trials--;
-					life.lastElementChild.remove();
-					step = 0;
-					return positionFruit();
-				}
-				return gameOverF();
-			}
-		}, 10)
-
+		fruitDown();
+		fruit.addEventListener('mouseover', fruitSlice)
 
 	}, 1250)
 }
 
-function AnimateInstr() {
+function animateInstr() {
 	instruction.classList.add('active');
 	instruction.animate([{
 		transform: 'translateX(-100%) scale(0.3)',
@@ -68,8 +69,7 @@ function AnimateInstr() {
 
 function styleStart() {
 	start.querySelector('p').textContent = 'Reset Game';
-	start.style.transform = 'scale(0.5)';
-	start.style.bottom = '0%';
+	start.classList.add('active');
 }
 
 function pickFruit() {
@@ -92,10 +92,47 @@ function positionFruit() {
 	// $(#game).append(`<img class="fruit" src="${pickFruit()}" >`)  -  This method allows to appear more than one fruit in game at the same time
 }
 
-function gameOverF() {
+function fruitDown() {
+	let randStep = 2 + Math.round(Math.random()*(1 + Math.round(Math.random()*9)));
+	let fruitDown = setInterval(() => {
+		step += randStep;
+		fruit.style.top = `${-fruit.offsetHeight + step}px`;
+		if (fruit.offsetTop >= game.offsetHeight) {
+			if (trials > 1) {
+				trials--;
+				life.lastElementChild.remove();
+				return newFruit();
+			}
+			life.lastElementChild.remove();
+			clearInterval(fruitDown)
+			gameOverF();
+		}
+	}, 10)
+}
 
+function newFruit() {
+	step = 0;
+	randStep = 2 + Math.round(Math.random()*(1 + Math.round(Math.random()*9)));
+	positionFruit();
+}
+
+function fruitSlice() {
+	scoreCount++;
+	scoreVal.textContent = scoreCount;
+	slicesound.currentTime = 0;
+	slicesound.play();
+	$(".fruit").hide("explode", 500);
+	setTimeout(() => {newFruit(); $(".fruit").show()}, 500) ;
+}
+
+function gameOverF() {
+	gameover.classList.add('active');
+	game.style.filter = 'grayscale(30%)';
+	start.querySelector('p').textContent = 'Start Game';
+	start.classList.remove('active');
 }
 
 start.addEventListener('click', startGame)
+
 
 
