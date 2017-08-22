f9fe7d7a648d3aba7abeff10b8b524ba28def704
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-21 23:10:29 -0300

    Capstone Project - All 2014/15

--- Python Capstone Project/dataUSA.py
+++ Python Capstone Project/dataUSA.py
@@ -21,13 +21,13 @@ for row in avgwage:
     rownum += 1
 conn.commit()
 
-# For all occupations:
+# For ALL occupations:
 # Sum of people in analysis per year
 allppl14 = 0
 for row in cur.execute('SELECT ppl2014 FROM AverageWage'):
     allppl14 += row[0]
-allppl15 = 0
 
+allppl15 = 0
 for row in cur.execute('SELECT ppl2015 FROM AverageWage'):
     allppl15 += row[0]
 
@@ -53,8 +53,53 @@ for row in cur.execute('SELECT avg2015, ppl2015 FROM AverageWage'):
             pplwage15[20] += row[1]
             break
 
+
+# For COMPUTER SCIENCE (and related) occupations:
+# Sum of people in analysis per year
+csppl14 = 0
+for row in cur.execute('SELECT occ, ppl2014 FROM AverageWage'):
+    if re.match('computer|software|information|web|database', row[0], re.I) :
+        csppl14 += row[1]
+
+csppl15 = 0
+for row in cur.execute('SELECT occ, ppl2015 FROM AverageWage'):
+    if re.match('computer|software|information|web|database', row[0], re.I) :
+        csppl15 += row[1]
+
+
+# Sum of people per range of wage
+cswage14 = [0] * 21
+for row in cur.execute('SELECT occ, avg2014, ppl2014 FROM AverageWage'):
+    if re.match('computer|software|information|web|database', row[0], re.I) :
+        for i in range(20) :
+            if row[1] < 10000*(i+1) : 
+                cswage14[i] += row[2]
+                break
+            if row[1] >= 200000 : 
+                cswage14[20] += row[2]
+                break
+
+cswage15 = [0] * 21
+for row in cur.execute('SELECT occ, avg2015, ppl2015 FROM AverageWage'):
+    if re.match('computer|software|information|web|database', row[0], re.I) :
+        for i in range(20) :
+            if row[1] < 10000*(i+1) : 
+                cswage15[i] += row[2]
+                break
+            if row[1] >= 200000 : 
+                cswage15[20] += row[2]
+                break
+
+
 cur.close()
 
+
+wageRange = ['<10K', '10-20K', '20-30K', '30-40K', '40-50K', '50-60K', '60-70K', '70-80K', '80-90K', 
+    '90-100K', '100-110K', '110-120K', '120-130K', '130-140K', '140-150K', '150-160K', '160-170K', 
+    '170-180K', '180-190K', '190-200K', '>200K']
+
+
+# For ALL occupations:
 # Calculating % share of each wage range
 for i in range(len(pplwage14)):
     pplwage14[i] = pplwage14[i]/allppl14 * 100
@@ -62,18 +107,37 @@ for i in range(len(pplwage14)):
 for i in range(len(pplwage15)):
     pplwage15[i] = pplwage15[i]/allppl15 * 100
 
+# For COMPUTER SCIENCE (and related) occupations:
+# Calculating % share of each wage range
+for i in range(len(cswage14)):
+    cswage14[i] = cswage14[i]/csppl14 * 100
 
-wageRange = ['<10K', '10-20K', '20-30K', '30-40K', '40-50K', '50-60K', '60-70K', '70-80K', '80-90K', 
-    '90-100K', '100-110K', '110-120K', '120-130K', '130-140K', '140-150K', '150-160K', '160-170K', 
-    '170-180K', '180-190K', '190-200K', '>200K']
+for i in range(len(cswage15)):
+    cswage15[i] = cswage15[i]/csppl15 * 100
 
-# Creating a JS file to be loaded by .htm file
-fhand = open('dataUSAline.js','w')
-fhand.write("dataUSAline = [ ['Wage Ranges', '2014', '2015']")
+
+# For 2014:
+# Creating a JS file to be loaded by .html file
+fhand4 = open('dataUSA2014.js','w')
+fhand4.write("dataUSA2014 = [ ['Wage Ranges', 'USA', 'Computer Science']")
 
 for i in range(len(pplwage14)):
-    fhand.write(",\n['" + wageRange[i] + "'," + str(round(pplwage14[i],4)) + ',' + str(round(pplwage15[i],4)) + "]")
-fhand.write("\n];\n")
+    fhand4.write(",\n['" + wageRange[i] + "'," + str(round(pplwage14[i],4)) + ',' + str(round(cswage14[i],4)) + "]")
+fhand4.write("\n];\n")
+
+
+# For 2015:
+# Creating a JS file to be loaded by .html file
+fhand5 = open('dataUSA2015.js','w')
+fhand5.write("dataUSA2015 = [ ['Wage Ranges', 'USA', 'Computer Science']")
+
+for i in range(len(pplwage15)):
+    fhand5.write(",\n['" + wageRange[i] + "'," + str(round(pplwage15[i],4)) + ',' + str(round(cswage15[i],4)) + "]")
+fhand5.write("\n];\n")
 
 
 
+print(allppl14) 
+print(pplwage14)
+print(csppl14)
+print(cswage14)
