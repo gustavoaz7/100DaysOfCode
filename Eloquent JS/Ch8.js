86256f594cbd87ddf8bfa79d772e7c2fbb478675
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-23 15:34:57 -0300

    Eloquent JS - Ch8

---
+++ Eloquent JS/Ch8.js
@@ -0,0 +1,10 @@
+// Strict Mode
+/*
+This mode is enabled by typing "use strict" at the of a file or a function body.
+This mode avoids JavaScript flexible interpretation
+Examples of what changes in strict mode:
+False is not equal 0 (nor true is 1)
+'this' is undefined when the function is not called as a method
+"this is the example number " + 3
+*/
+
