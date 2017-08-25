18c6c54ba6ea251ebcd6abe626fd61fb158a0f63
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-25 11:39:24 -0300

    Eloquent JS - Ch12

---
+++ Eloquent JS/Ch12.js
@@ -0,0 +1,10 @@
+/* Networks and Internet
+    TCP - Transmission Control Protocol
+        Server - Computer listening
+        Client - Computer connecting
+        Port - number related to a specific type of protocol (Web - port 80 - HTTP)
+    URL - Uniform Resource Locator
+        http://eloquentjavascript.net/12_browser.html
+        |      |                      |               |
+        protocol      server (IP)           path
+    
