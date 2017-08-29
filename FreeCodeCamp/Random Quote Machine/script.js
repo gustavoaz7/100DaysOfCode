db8109b684b70c559aeb734e46707f9bb3dc6d75
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-28 23:22:59 -0300

    FCC Random Quote Machine

---
+++ FreeCodeCamp/Random Quote Machine/script.js
@@ -0,0 +1,28 @@
+
+var quoteMsge = $("#btn");
+
+$(document).ready(function() {
+  quoteMsge.click(function () {
+    $.ajax({
+			url: "https://api.forismatic.com/api/1.0/",
+			jsonp: "jsonp",
+			dataType: "jsonp",
+			data: {
+				method: "getQuote",
+				lang: "en",
+				format: "jsonp"
+			},
+			success: function(quote) {
+				$("#quote").text(quote.quoteText);
+				$("#author").text(quote.quoteAuthor || 'Anonymmous');
+				var tweet = quote.quoteText+"\nBy: "+ (quote.quoteAuthor || 'Anonymous');
+				$('#twitter').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweet));       
+				if (tweet.length > 140) $('#twitter').click((e) => {
+					alert('Tweet is not possible. This quote is over 140 characters.'); 
+					e.preventDefault()
+				});
+			}
+    });
+  });
+});
+
