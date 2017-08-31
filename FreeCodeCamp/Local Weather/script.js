f3dafcd55ffd4b47f431cef2d732080b44b45379
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-31 12:23:37 -0300

    Local Weather
    Added weather info

--- FreeCodeCamp/Local Weather/script.js
+++ FreeCodeCamp/Local Weather/script.js
@@ -2,12 +2,27 @@ const city = document.querySelector('.city');
 const country = document.querySelector('.country')
 const hour = document.querySelector('.hour')
 const week = document.querySelector('.week')
-const weather = document.querySelector('.weather')
+const weatherIcon = document.querySelector('.weather > i')
 const tempNow = document.querySelector('.tempNow')
 const tempMaxMin = document.querySelector('.tempMaxMin')
+const humidity = document.querySelector('.humidity')
+const cloudyness = document.querySelector('.cloudyness')
+const wind = document.querySelector('.wind')
+//document.querySelector('*').style.color = '#eee';
 const info = document.querySelector('.info')
+const forecast = document.querySelector('.forecast')
 
 const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const weather = {
+  'Thunderstorm': 'wi-thunderstorm',
+  'Drizzle': 'wi-sprinkle',
+  'Rain': 'wi-rain',
+  'Snow': 'wi-snow',
+  'Atmosphere': 'wi-dust',
+  '800': ['wi-day-sunny', 'wi-night-clear'],
+  '8xx': {'fc': ['wi-day-cloudy', 'wi-night-alt-cloudy'], 'other': 'wi-cloudy'},
+  'xxx': 'wi-na'
+}
 
 setInterval(function() {
   let now = new Date();
@@ -16,7 +31,7 @@ setInterval(function() {
   if ((month+"").length === 1) {month = `0${now.getMonth()+1}`};
   week.innerHTML = `${now.toString().split(" ")[0]} ${month}/${now.getDate()}`;
 }, 1000);
-/*
+
 // Getting Geolocation
 
 const geoURL = 'https://freegeoip.net/json/';
@@ -35,7 +50,27 @@ let lat, lon;
     console.log(weatherURL);
     (fetch(weatherURL).then(response => response.json())
       .then(data => {
-        console.log(data);
+        tempNow.innerHTML  = `<p>${data.main.temp.toFixed(1)}</p><i class="wi wi-celsius sm"></i>`;
+        tempMaxMin.innerHTML = `<p>${data.main.temp_max}</p><i class="wi wi-degrees sm"></i> <p>${data.main.temp_min}</p><i class="wi wi-degrees sm"></i>`;
+        humidity.innerHTML = `${data.main.humidity}%`;
+        cloudyness.innerHTML = `${data.clouds.all}%`;
+        wind.innerHTML = `${data.wind.speed*3.6} km/h`;
+        
+        // Selecting the right icon according to the weather
+        let main = data.weather[0].main;
+        let id = data.weather[0].id;
+        let icon = data.weather[0].icon;
+        let descript = data.weather[0].description;
+        let actual;
+        if (weather[main]) {actual = weather[main]
+        } else if (id === 800) { actual = (
+            icon.includes('d') ? weather[800][0] : weather[800][1] )
+        } else if (id < 900) { actual = (
+            descript.includes('few clouds') ? 
+              icon.includes('d') ? weather['8xx']['fc'][0] : weather['8xx']['fc'][1] :
+            weather['8xx']['other'] )
+        } else {actual = weather['xxx']}
+        weatherIcon.classList.add(actual);
     })
     );
   })
@@ -83,6 +118,14 @@ name City name
 cod Internal parameter
 
 
+<i class="wi wi-night-sleet"></i>
+
+tempMax:  wi-thermometer
+tempMin:  wi-thermometer-exterior
+celsius:  wi-celsius
+fahrenheit:  wi-fahrenheit
+
+
 
 
 
@@ -90,22 +133,24 @@ id 2xx - Thunderstorm (unique icon)
     wi-thunderstorm
 
 id 3xx - Drizzle (unique icon)
+    wi-sprinkle
 
-
-id 5xx - Rain (day icon / 'shower rain' cloud+rain icon)
-
+id 5xx - Rain (unique icon)
+    wi-rain
 
 id 6xx - Snow (unique icon)
-
+    wi-snow
 
 id 7xx - Atmosphere (unique icon - ~mist)
-
+    wi-dust
 
 id 800 - Clear (day icon / night icon)
     wi-day-sunny      wi-night-clear
 
 id 80x - Clouds ('few clouds' day/night  --  others unique icon)
-
+    wi-day-cloudy   wi-night-alt-cloudy         wi-cloudy
 
 id 9xx - Extreme / Additional
+    wi-na
+
 */
