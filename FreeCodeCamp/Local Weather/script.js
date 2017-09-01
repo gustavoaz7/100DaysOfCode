a1fd71031943fe4e70edbaa903fc3e697639f3ae
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-08-31 23:18:55 -0300

    Local Weather
    Chained promises
    Created function for current weather icon&background
    Added 5 day forecast

--- FreeCodeCamp/Local Weather/script.js
+++ FreeCodeCamp/Local Weather/script.js
@@ -14,14 +14,14 @@ const forecast = document.querySelector('.forecast')
 
 const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const weather = {
-  'Thunderstorm': 'wi-thunderstorm',
-  'Drizzle': 'wi-sprinkle',
-  'Rain': 'wi-rain',
-  'Snow': 'wi-snow',
-  'Atmosphere': 'wi-dust',
-  '800': ['wi-day-sunny', 'wi-night-clear'],
-  '8xx': {'fc': ['wi-day-cloudy', 'wi-night-alt-cloudy'], 'other': 'wi-cloudy'},
-  'xxx': 'wi-na'
+  'Thunderstorm': ['wi-thunderstorm', 'url'],
+  'Drizzle': ['wi-sprinkle', 'url'],
+  'Rain': ['wi-rain', 'url'],
+  'Snow': ['wi-snow', 'url'],
+  'Atmosphere': ['wi-dust', 'url'],
+  '800': [['wi-day-sunny', 'url'], ['wi-night-clear', 'url']],
+  '8xx': {'fc': [['wi-day-cloudy', 'url'], ['wi-night-alt-cloudy', 'url']], 'other': ['wi-cloudy', 'url']},
+  'xxx': ['wi-na', 'url']
 }
 
 setInterval(function() {
@@ -33,124 +33,86 @@ setInterval(function() {
 }, 1000);
 
 // Getting Geolocation
-
+const key = '89232155a28e10f6f080b65258157ece';
 const geoURL = 'https://freegeoip.net/json/';
 let lat, lon;
+(fetch(geoURL)
+  .then(response => response.json())
+    .then(data => {
+      lat = data.latitude;
+      lon = data.longitude;
+      city.innerHTML = data.city;
+      country.innerHTML = `${data.region_code}, ${data.country_name}`;
 
-(fetch(geoURL).then(response => response.json())
-  .then(data => {
-    lat = data.latitude;
-    lon = data.longitude;
-    city.innerHTML = data.city;
-    country.innerHTML = `${data.region_code}, ${data.country_name}`;
-
-    // Getting Weather Information
-    const key = '89232155a28e10f6f080b65258157ece';
-    let weatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${key}`
-    console.log(weatherURL);
-    (fetch(weatherURL).then(response => response.json())
-      .then(data => {
-        tempNow.innerHTML  = `<p>${data.main.temp.toFixed(1)}</p><i class="wi wi-celsius sm"></i>`;
-        tempMaxMin.innerHTML = `<p>${data.main.temp_max}</p><i class="wi wi-degrees sm"></i> <p>${data.main.temp_min}</p><i class="wi wi-degrees sm"></i>`;
-        humidity.innerHTML = `${data.main.humidity}%`;
-        cloudyness.innerHTML = `${data.clouds.all}%`;
-        wind.innerHTML = `${data.wind.speed*3.6} km/h`;
-        
-        // Selecting the right icon according to the weather
-        let main = data.weather[0].main;
-        let id = data.weather[0].id;
-        let icon = data.weather[0].icon;
-        let descript = data.weather[0].description;
-        let actual;
-        if (weather[main]) {actual = weather[main]
-        } else if (id === 800) { actual = (
-            icon.includes('d') ? weather[800][0] : weather[800][1] )
-        } else if (id < 900) { actual = (
-            descript.includes('few clouds') ? 
-              icon.includes('d') ? weather['8xx']['fc'][0] : weather['8xx']['fc'][1] :
-            weather['8xx']['other'] )
-        } else {actual = weather['xxx']}
-        weatherIcon.classList.add(actual);
+      // Getting Weather Information
+      const weatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${key}`;
+      return fetch(weatherURL);
     })
-    );
-  })
-);
+  .then(response => response.json())
+    .then(data => {
+      tempNow.innerHTML  = `<p>${data.main.temp.toFixed(1)}</p><i class="wi wi-celsius sm"></i>`;
+      tempMaxMin.innerHTML = `<p>${data.main.temp_max}</p><i class="wi wi-degrees sm"></i> <p>${data.main.temp_min}</p><i class="wi wi-degrees sm"></i>`;
+      humidity.innerHTML = `${data.main.humidity}%`;
+      cloudyness.innerHTML = `${data.clouds.all}%`;
+      wind.innerHTML = `${(data.wind.speed*3.6).toFixed(1)} km/h`;
+      
+      // Selecting icon and Background according to the weather
+      let main = data.weather[0].main;
+      let id = data.weather[0].id;
+      let icon = data.weather[0].icon;
+      let descript = data.weather[0].description;
+      let [i, bg] = getIconAndBackground(main, id, icon, descript);
+      weatherIcon.classList.add(i);
 
+      // Getting 5 Day Forecast
+      let forecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&APPID=${key}`;
+      return fetch(forecastURL);
+    })
+    .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let now = new Date();
+        let wd = now.getDay();
+        for (let i = 0; i < 5; i++) {
+          let div = document.createElement('div');
+            div.classList.add('fore');
+            div.classList.add(`f${i}`);
+          let day = document.createElement('p');
+            wd++;
+            // Back to Sunday after 7 days
+            if (wd > 6) wd -= 7;
+            day.textContent = weekday[wd];
+          let ico = document.createElement('i');
+            // Timespan of 3h between every item in the list. Therefore i*8
+            let main = data.list[i*8].weather[0].main;
+            let id = data.list[i*8].weather[0].id;
+            let icon = data.list[i*8].weather[0].icon;
+            let descript = data.list[i*8].weather[0].description;
+            let [ic, bg] = getIconAndBackground(main, id, icon, descript);
+            ico.classList.add('wi');
+            ico.classList.add(ic);
+          let tem = document.createElement('span');
+          div.appendChild(day);
+          div.appendChild(ico);
+          div.appendChild(tem);
+          forecast.appendChild(div);
+          document.querySelector(`.f${i} > span`).innerHTML = data.list[i*8].main.temp.toFixed(1);
+        };
 
+      })
+)
 
-/*
-coord
-coord.lon City geo location, longitude
-coord.lat City geo location, latitude
-weather (more info Weather condition codes)
-weather.id Weather condition id
-weather.main Group of weather parameters (Rain, Snow, Extreme etc.)
-weather.description Weather condition within the group
-weather.icon Weather icon id
-base Internal parameter
-main
-main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
-main.pressure Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
-main.humidity Humidity, %
-main.temp_min Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
-main.temp_max Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
-main.sea_level Atmospheric pressure on the sea level, hPa
-main.grnd_level Atmospheric pressure on the ground level, hPa
-wind
-wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
-wind.deg Wind direction, degrees (meteorological)
-clouds
-clouds.all Cloudiness, %
-rain
-rain.3h Rain volume for the last 3 hours
-snow
-snow.3h Snow volume for the last 3 hours
-dt Time of data calculation, unix, UTC
-sys
-sys.type Internal parameter
-sys.id Internal parameter
-sys.message Internal parameter
-sys.country Country code (GB, JP etc.)
-sys.sunrise Sunrise time, unix, UTC
-sys.sunset Sunset time, unix, UTC
-id City ID
-name City name
-cod Internal parameter
+function getIconAndBackground(main, id, icon, descript) {
+  let sel;
+  if (weather[main]) {return [weather[main][0], weather[main][1]];
+  } else if (id === 800) {
+      sel = weather[800];
+      return icon.includes('d') ? [sel[0][0], sel[0][1]] : [sel[1][0], sel[1][1]];
+  } else if (id < 900) {
+      sel = weather['8xx'];
+      return descript.includes('few clouds') ? 
+        icon.includes('d') ? [sel.fc[0][0], sel.fc[0][1]] : [sel.fc[1][0], sel.fc[1][1]] :
+      [sel.other[0], sel.other[1]];
+  } else {return [weather.xxx[0], weather.xxx[1]];}
+}
 
-
-<i class="wi wi-night-sleet"></i>
-
-tempMax:  wi-thermometer
-tempMin:  wi-thermometer-exterior
-celsius:  wi-celsius
-fahrenheit:  wi-fahrenheit
-
-
-
-
-
-id 2xx - Thunderstorm (unique icon)
-    wi-thunderstorm
-
-id 3xx - Drizzle (unique icon)
-    wi-sprinkle
-
-id 5xx - Rain (unique icon)
-    wi-rain
-
-id 6xx - Snow (unique icon)
-    wi-snow
-
-id 7xx - Atmosphere (unique icon - ~mist)
-    wi-dust
-
-id 800 - Clear (day icon / night icon)
-    wi-day-sunny      wi-night-clear
-
-id 80x - Clouds ('few clouds' day/night  --  others unique icon)
-    wi-day-cloudy   wi-night-alt-cloudy         wi-cloudy
-
-id 9xx - Extreme / Additional
-    wi-na
-
-*/
