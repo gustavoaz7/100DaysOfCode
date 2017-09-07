045d682ea044df77827cd83fc4185219bf62fa16
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-09-07 17:59:14 -0300

    TwitchTV

--- FreeCodeCamp/TwitchTV/script.js
+++ FreeCodeCamp/TwitchTV/script.js
@@ -1,25 +1,51 @@
-const userIDs = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", 'garenatw', 'silvername', 'nooneboss'];
+const userIDs = ["ESL_SC2", 'c9shroud', 'LIRIK', 'summit1g', "OgamingSC2", "cretetion", "freecodecamp", 
+  "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", 'garenatw', 'silvername', 'nooneboss'];
 const key = '6uwh8hhynrxszcavv8ksp03v0dj5iq';
-let users = [];
+let users = [];  // Array of objects(channels)
+const filters = document.querySelectorAll('#head>li');
 const update = document.querySelector('#update');
 const list = document.querySelector('#list');
 
+// Fetching channel list from server on page load
 getChannels();
 
-// If checked: Automatically update every 30s
-update.addEventListener('change', () => {
-  if (update.checked) {
-    setInterval(getChannels, 30000);
-  }
-})
+// Filtering list of channels 
+filters.forEach(li => li.addEventListener('click', function () {
+  // Styling filter button
+  filters.forEach(l=>l.classList.remove('active'));
+  li.classList.add('active');
+  // Filtering list
+  li.id === 'on' ? populateList(users.filter(user => user.online)) :
+  li.id === 'off' ? populateList(users.filter(user => !user.online)) : populateList(users);
+}))
+
 
 function populateList (array) {
   // Cleaning list before populating it
   while (list.firstChild) {list.removeChild(list.firstChild)};
-  
+  // Inserting each channel with its informations
   for (let i = 0; i < array.length; i++) {
-    // STYLE FIRST THAN CONTINUElist.innerHTML += `<li>`
+  list.innerHTML += `
+<a href="${array[i].url}" target="_blank">
+<div>
+<img class="logoimg" src="${array[i].logo}" alt="logo">
+<h3>${array[i].name}</h3>
+<span>${array[i].online ? 'Online' : 'Offline'}</span>
+<p id="game">${array[i].game || ""}</p>
+<p id="lang">Lang: ${array[i].language || '-'}</p>
+</div>
+</a>`;
   }
+  // Styling text and background if online/offline
+  document.querySelectorAll('span').forEach(span => {
+    if (span.textContent === 'Online') {
+      span.style.color = 'green';
+      span.parentElement.style.background = 'rgba(135, 211, 124, 0.3)';
+    } else if (span.textContent === 'Offline') {
+      span.style.color = 'red';
+      span.parentElement.style.background = 'rgba(236,100,75, 0.2)';
+    }
+  })
 }
 
 
@@ -32,26 +58,21 @@ function getChannels() {
         users[i] = {
           name: userIDs[i],
           online: false,
-          logo: 'http://www.offworldgames.com/wp-content/uploads/2015/07/twitch.jpeg'
+          logo: 'http://www.offworldgames.com/wp-content/uploads/2015/07/twitch.jpeg',
+          url: 'https://www.twitch.tv/'
         }
       } else {
         users[i] = {
           name: userIDs[i],
           online: true,
           game: data.stream.channel.game,
-          language: data.stream.channel.language,
-          followers: data.stream.channel.followers,
+          language: data.stream.channel.language.toUpperCase(),
+          logo: data.stream.channel.logo,
           url: data.stream.channel.url
         }
       }
+      return users;
     })
+    .then(users => populateList(users))
   }); 
 }
-
-/*
-http://www.offworldgames.com/wp-content/uploads/2015/07/twitch.jpeg
-
-https://www.twitch.tv/p/assets/uploads/glitch_474x356.png
-
-https://pbs.twimg.com/profile_images/509073338191183872/fYdty6yd.png
-*/
